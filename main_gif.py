a0316352e5b9936fc1161e7879886b990b2d1627
Yeesou <autretpierrick795@gmail.com>
2025-09-19 16:05:44 +0200

    Ajustement du nombre de villes à 100 et modification de la durée de l'animation à 10 secondes

--- main_gif.py
+++ main_gif.py
@@ -2,18 +2,18 @@ from Groupes import Groupes
 
 import matplotlib.pyplot as plt
 
-cities = 250
+cities = 100
 indiv_length = cities * 5
 tournament_size = indiv_length // 10
 mutation_rate = 1
 
 France = Groupes(indiv_length, tournament_size, mutation_rate)
-France.generate_circle_city(cities)
-#France.generate_random_city(cities)
+#France.generate_circle_city(cities)
+France.generate_random_city(cities)
 France.generate_individus()
 
 print("Démarrage de l'animation...")
-anim = France.animate_evolution(generations=5000, interval=5, method='hx extended', mutation='2-opt alt', duration=20)
+anim = France.animate_evolution(generations=5000, interval=5, method='hx extended', mutation='2-opt alt', duration=10)
 
 if anim:
     plt.show()
